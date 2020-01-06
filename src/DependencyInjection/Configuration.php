1f954b36d2de2fb1a8408d5dc2ee6f71c3381dfd
Martin Kirilov <wucdbm@gmail.com>
2020-01-06 02:14:21 +0200

    drop support for sf3, fix deprecation in 4.3, support for 5.0

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,8 +19,8 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 class Configuration implements ConfigurationInterface {
 
     public function getConfigTreeBuilder() {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('forci_locale');
+        $treeBuilder = new TreeBuilder('forci_locale');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
